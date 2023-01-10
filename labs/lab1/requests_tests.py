043a1855cd83281c4911c29c176ead74317d80cd
MHUalberta <mohammadhammad50@gmail.com>
2023-01-09 22:01:40 -0700

    Finish Lab 1

--- labs/lab1/requests_tests.py
+++ labs/lab1/requests_tests.py
@@ -1,6 +1,15 @@
 import requests
 
+#Get version
 print(requests.__version__)
+print()
 
-response = requests.get("https://www.google.com/")
-print(response)
+#GET request
+response1 = requests.get("https://www.google.com/")
+print(response1)
+print()
+
+#Print from github
+response2 = requests.get("https://raw.githubusercontent.com/MHUalberta/CMPUT404/main/labs/lab1/requests_tests.py?token=GHSAT0AAAAAAB5BAD352OD6GUHRO4GTUWLSY546EDA")
+print(response2.text)
+print()
